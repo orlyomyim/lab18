cf129915bef90e593a41a174238c26b131ca8f7c
Your Name <you@example.com>
2023-02-14 17:01:52 +0700

    ez f

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,19 +1,5 @@
-#include<iostream>
-using namespace std;
-
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
-}
-
-int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
+void myString(char *&p ,int N){
+	p = new char[26];    
+	for(int i = 0; i < N;i++) p[i] = 'A'+i;
+	N = 0;
 }
